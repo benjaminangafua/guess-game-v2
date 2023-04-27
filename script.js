19d64f82dcb2f9bdca96e98e1fec6b080569da92
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2023-04-27 11:47:54 +0000

    Updated sound

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const guessBoard = document.getElementById("guess-board");
 const restart = document.getElementById("restart");
-let chance = document.getElementById("chance")
-let result = document.querySelector(".result")
+const chance = document.getElementById("chance")
+const result = document.querySelector(".result")
 let __ = [],count = 10
 
 const createBoard = ()=>{
@@ -13,7 +13,7 @@ const createBoard = ()=>{
     }
 
     for(let i = 0; i<__.length; i++){
-        let div = document.createElement("div")
+        const div = document.createElement("div")
         div.innerHTML = __[i]
         div.setAttribute("class", "num-guess")
         div.style.cursor="pointer"
@@ -22,38 +22,71 @@ const createBoard = ()=>{
 }
 createBoard()
 
-let guess = document.querySelectorAll(".num-guess")
+const guess = document.querySelectorAll(".num-guess")
 let random = Math.floor(Math.random()*100)+1
 chance.innerHTML = count
 
+const audio =[
+    "./audio/apostle.m4a",
+    "./audio/basic1plus1.m4a",
+    "./audio/chants.m4a",
+    "./audio/chants-2.m4a",
+    "./audio/cutting-grass.m4a",
+    "./audio/dinner-man.m4a",
+    "./audio/funke.m4a",
+    "./audio/jesus-is-lord.m4a",
+    "./audio/jezuz.m4a",
+    "./audio/macdonalds.m4a",
+    "./audio/no-future.m4a",
+    "./audio/persecuted.m4a",
+    "./audio/pregnancy.m4a",
+    "./audio/think-about-your-life.m4a",
+    "./audio/you-can-never-make-it-1.m4a",
+    "./audio/you-can-never-make-it-full.mp3",
+    "./audio/you-are-a-failure.m4a"
+]
+const voice=()=>(new Audio(audio[Math.floor(Math.random()*audio.length)]))
+    
 // Check For Win
-function checkForWin(event){
+const checkForWin =(event, removeEvent)=>{
     if(count >0){
-        result.innerHTML = event.target.innerHTML < random?"Too Low":
-        event.target.innerHTML > random?"Too High": "Win"
-        event.target.style = event.target.innerHTML == random?
-        "background: blue; color: white": "background: gray"
+        result.innerHTML =event.target.innerHTML < random?"Too Low":event.target.innerHTML > random?"Too High":"Congrats"
+        if(event.target.innerHTML != random){
+            event.target.style = `background:gray; cursor:not-allowed`
+            event.target.disabled=true;
+            
+            voice().play() 
+        }else if(event.target.innerHTML == random) {
+            event.target.style = `background: blue; color: white; cursor:not-allowed`
+            guess.forEach (ele=>{
+                ele.style = `cursor:not-allowed`
+                ele.replaceWith(ele.cloneNode(true))
+            })
+            console.log(event.target.innerHTML)
+            event.disabled=true;
+            voice().pause()
+        }
    }else{
         result.innerHTML =  "Time Elapses";
     }
 }
 
-function disableAfterPlay(event){  
+// Disable played number
+const disableAfterPlay = event=>{  
     count --
     chance.innerHTML = count   
     event.preventDefault()
     event.target.removeEventListener("click", disableAfterPlay) 
-    checkForWin(event)
+    checkForWin(event,  disableAfterPlay)
 }
-
-
 guess.forEach(ele=>ele.addEventListener("click", disableAfterPlay) )
  
 
 restart.addEventListener("click", ()=>{
     guess.forEach(element=>{
-        element.addEventListener("click", disableAfterPlay) 
-        element.classList.toggle("bg");
+        element.addEventListener("click", disableAfterPlay)
+        element.style.background = "none" 
+        // element.classList.toggle("bg");
         element.style.cursor="pointer";
     })
     random = Math.floor(Math.random()*100)+1;
